7d34c5e878c1e22616540d0af571bd1e5a77372f
Kohei KONISHI <konishi2911@icloud.com>
2020-12-03 12:39:24 +0900

    Initial Commit

---
+++ BudgetCoreDatabase/BudgetCoreDatabase.h
@@ -0,0 +1,18 @@
+//
+//  BudgetCoreDatabase.h
+//  BudgetCoreDatabase
+//
+//  Created by Kohei KONISHI on 2020/12/03.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BudgetCoreDatabase.
+FOUNDATION_EXPORT double BudgetCoreDatabaseVersionNumber;
+
+//! Project version string for BudgetCoreDatabase.
+FOUNDATION_EXPORT const unsigned char BudgetCoreDatabaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BudgetCoreDatabase/PublicHeader.h>
+
+
